7322ce1414c55744b8f2d8570df6acf5826ed6f7
Dahren Ericsson <dahrenericsson@proton.me>
2024-11-28 22:12:11 +0700

    test(register): Added test register with valid number, but it still non-functional

--- tests/test_register.py
+++ tests/test_register.py
@@ -4,7 +4,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from conftest import browser as BrowserConfig
+import pytest
 # Helper Function Section
 
 """
@@ -40,24 +40,58 @@ def check_terms_and_conditions_checkbox(browser: WebDriver):
     terms_and_conditions_checkbox = browser.find_element(by=By.XPATH, value="//label[contains(@class, 'iweb-checkbox')]")
     terms_and_conditions_checkbox.click()
 
-# Test Section
 """
-Test the registration process using an invalid phone number and checking the terms and conditions checkbox.
+Fixture to ensure before every test, the registration modal is opened.
 """
-def test_register_with_invalid_phone_number(browser: WebDriver) -> None:
-    current_url = browser.current_url
-
+@pytest.fixture(scope="function")
+def registration(browser: WebDriver):
     click_on_signup_link(browser)
+    yield browser
+
+# Test Section
+#TODO: Find a way to solve OTP, and reusing the phone number. Captcha is solved through extension.
+"""
+TC_01  Test the registration process using a valid phone number and checking the terms and conditions checkbox.
+Expect: The page should change to the next page.
+"""
+@pytest.mark.skip(reason="This test is unsolvable for now.")
+def test_register_with_valid_phone_number(registration: WebDriver) -> None:
+    current_url = registration.current_url
+
+    phone_number = "087750999479"
+    type_phone_number(registration, phone_number)
+    
+    check_terms_and_conditions_checkbox(registration)
+    
+    confirm_registration_with_whatsapp(registration)
+    
+    # Wait for maximum 10 minutes to solve captcha. If it's completed before 10 minutes, it will continue to the next step.
+    registration.implicitly_wait(600)
+    
+    # Wait for the page to change.
+    WebDriverWait(registration, 10).until(
+        EC.url_changes
+    )
+    
+    assert False, "This test is unsolvable for now."
+
+
+"""
+TC_02 Test the registration process using an invalid phone number and checking the terms and conditions checkbox.
+Expect: The page should not change.
+"""
+def test_register_with_invalid_phone_number(registration: WebDriver) -> None:
+    current_url = registration.current_url
 
     phone_number = "241424"
-    type_phone_number(browser, phone_number)
+    type_phone_number(registration, phone_number)
     
-    check_terms_and_conditions_checkbox(browser)
+    check_terms_and_conditions_checkbox(registration)
     
-    confirm_registration_with_whatsapp(browser)
+    confirm_registration_with_whatsapp(registration)
 
     # Find the error toast.
-    invalid_phone_number_toast: WebElement = WebDriverWait(browser, 10).until(
+    invalid_phone_number_toast: WebElement = WebDriverWait(registration, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "[class*='iweb-toast']"))
     )
     
@@ -66,28 +100,27 @@ def test_register_with_invalid_phone_number(browser: WebDriver) -> None:
     # Assert that the toast message is correct.
     assert invalid_phone_number_toast.text == "Please enter a valid phone number.", "Toast message should be 'Please enter a valid phone number.'"
     # Assert that we dont change page.
-    assert browser.current_url == current_url
+    assert registration.current_url == current_url
     
     sleep(1)
 
 """
-Test the registration process using either a valid or invalid phone number without checking the terms and conditions checkbox.
+TC_03 Test the registration process using either a valid or invalid phone number without checking the terms and conditions checkbox.
+Expect: The page should not change.
 """
-def test_register_with_unchecked_terms_and_conditions(browser: WebDriver) -> None:
+def test_register_with_unchecked_terms_and_conditions(registration: WebDriver) -> None:
     # Get the current url now to test that we dont change page.
-    current_url = browser.current_url
+    current_url = registration.current_url
 
-    # Make the registration modal pop up.
-    click_on_signup_link(browser)
     # Wait for registration modal to appear
     # Look for the phone number field inside the registration modal.
     # Wait until it appear..
     phone_number = "081331413699"
-    type_phone_number(browser, phone_number)
-    confirm_registration_with_whatsapp(browser)
+    type_phone_number(registration, phone_number)
+    confirm_registration_with_whatsapp(registration)
 
     # Find the error toast.
-    terms_and_conditions_toast: WebElement = WebDriverWait(browser, 10).until(
+    terms_and_conditions_toast: WebElement = WebDriverWait(registration, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "[class*='iweb-toast']"))
     )
     
@@ -96,6 +129,6 @@ def test_register_with_unchecked_terms_and_conditions(browser: WebDriver) -> Non
     # Assert that the toast message is correct.
     assert terms_and_conditions_toast.text == "You should agree to our Terms of Use and Privacy Policy.", "Toast message should be 'You should agree to our Terms of Use and Privacy Policy.'"
     # Assert that we dont change page.
-    assert browser.current_url == current_url, "URL should still stay the same."
+    assert registration.current_url == current_url, "URL should still stay the same."
 
     sleep(1)
