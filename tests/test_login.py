8c430902e56275f952868d6af1675a3862bf4d0f
Dahren Ericsson <dahrenericsson@proton.me>
2024-12-03 13:26:35 +0700

    chore(login): Changed the login test from function based into class based

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,5 @@
-from selenium.webdriver.chrome.webdriver import WebDriver 
+import pytest
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from time import sleep
@@ -6,92 +7,131 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 
-def test_login_with_facebook(browser: WebDriver):
-    # Navigate to the login page
-    print("Navigating to Lazada homepage...")
-    browser.get("https://www.lazada.co.id/")
-    sleep(3)  # Slow down the process
 
-    # Wait for the login button to appear and click it
-    print("Waiting for the login button to appear...")
-    login_button: WebElement = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="anonLogin"]/a'))
-    )
-    print("Clicking the login button...")
-    login_button.click()
-    sleep(3)  # Slow down the process
+class BaseTestLogin:
+    browser: WebDriver
 
-    # Wait for the Facebook login button to appear and click it
-    print("Waiting for the Facebook login button to appear...")
-    facebook_login_button: WebElement = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//span[text()='Facebook']"))
-    )
-    print("Clicking the Facebook login button...")
-    facebook_login_button.click()
-    sleep(3)  # Slow down the process
+    @pytest.fixture(scope="function", autouse=True)
+    def login(self, browser: WebDriver):
+        """
+        Fixture to ensure before every test, the login modal is opened.
+        :param browser: browser should automatically injected from conftest.py
+        :return: WebDriver - The browser instance.
+        """
+        self.browser = browser
+        self._click_on_login_link()
+        yield browser
 
-    # Switch to the Facebook login window
-    print("Switching to the Facebook login window...")
-    WebDriverWait(browser, 10).until(lambda d: len(d.window_handles) > 1)
-    browser.switch_to.window(browser.window_handles[1])
-    sleep(3)  # Slow down the process
+    def _click_on_login_link(self):
+        """
+        Click on the login link to pop up the login modal.
+        :return: None
+        """
+        login_link: WebElement = self.browser.find_element(by=By.ID, value="anonLogin")
+        login_link.click()
 
-    # Enter the email
-    print("Entering the email...")
-    email_field: WebElement = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "email"))
-    )
-    email_field.send_keys(os.getenv("FACEBOOK_EMAIL"))
-    sleep(3)  # Slow down the process
 
-    # Enter the password
-    print("Entering the password...")
-    password_field: WebElement = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "pass"))
-    )
-    password_field.send_keys(os.getenv("FACEBOOK_PASSWORD"))
-    sleep(3)  # Slow down the process
+class TestLocalLogin(BaseTestLogin):
+    pass
 
-    # Click the login button
-    print("Clicking the login button...")
-    login_button: WebElement = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.NAME, "login"))
-    )
-    login_button.click()
-    sleep(3)  # Slow down the process
 
-    # Wait for the account selection to appear and select the registered account
-        # Wait for the "Lanjutkan sebagai" button to appear and click it
-    print("Waiting for the 'Lanjutkan sebagai' button to appear...")
-    lanjutkan_button: WebElement = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(normalize-space(.), 'Lanjutkan sebagai')]"))
-    )
-    print("Clicking the 'Lanjutkan sebagai' button...")
-    lanjutkan_button.click()
-    sleep(3)  # Slow down the process
+class TestGoogleLogin(BaseTestLogin):
+    pass
 
-    # //span[contains(normalize-space(.), 'Lanjutkan sebagai')]
-    # account_selection: WebElement = WebDriverWait(browser, 10).until(
-    #     EC.presence_of_element_located((By.XPATH, f"//div[contains(text(), '{os.getenv("FACEBOOK_EMAIL")}')]"))
-    # )
-    print("Selecting the registered account...")
-    # account_selection.click()
-    sleep(3)  # Slow down the process
 
-    # Switch back to the Lazada window
-    print("Switching back to the Lazada window...")
-    browser.switch_to.window(browser.window_handles[0])
-    sleep(3)  # Slow down the process
+class TestFacebookLogin(BaseTestLogin):
+    @pytest.fixture(scope="function", autouse=True)
+    def _facebook_login(self, login: WebDriver):
+        self._click_on_facebook_login_button()
+        self._switch_to_facebook_window()
 
-    # Wait for the homepage to load and verify the login
+    def _click_on_facebook_login_button(self):
+        """
+        Click on the Facebook login button.
+        :return: None
+        """
+        facebook_login_button: WebElement = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[text()='Facebook']"))
+        )
+        facebook_login_button.click()
 
-    # BUG: Kayaknya yang ini problematik, soalnya text Welcome back ga bakal muncul.
-    print("Waiting for the homepage to load...")
-    WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Welcome back')]"))
-    )
+    def _switch_window(self, window_index: int):
+        """
+        Switch to the window with the specified index.
+        :param window_index: the index of the window.
+        :return: None
+        """
+        self.browser.switch_to.window(self.browser.window_handles[window_index])
 
-    # Assert that the user is on the homepage
-    print("Verifying the login...")
-    assert "https://www.lazada.co.id/" in browser.current_url, "User should be redirected to the homepage"
-    print("Test completed successfully.")
+    def _switch_to_facebook_window(self):
+        """
+        Switch to the Facebook login window.
+        :return: None
+        """
+        # Wait until the facebook login window pop up.
+        WebDriverWait(self.browser, 10).until(lambda d: len(d.window_handles) > 1)
+        self._switch_window(1)
+
+    def _type_facebook_email(self, email: str):
+        """
+        Type the email into the email field.
+        :param email: the facebook user's email
+        :return: None
+        """
+        email_field: WebElement = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "email"))
+        )
+        email_field.send_keys(os.getenv(email))
+
+    def _type_facebook_password(self, password: str):
+        """
+        Type the password into the password field.
+        :param password: the facebook user's password
+        :return: None
+        """
+        password_field: WebElement = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "pass"))
+        )
+        password_field.send_keys(os.getenv(password))
+
+    def _click_on_login_button(self):
+        """
+        Click on the login button IN THE FACEBOOK MODAL.
+        :return: None
+        """
+        login_button: WebElement = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.NAME, "login"))
+        )
+        login_button.click()
+
+    def test_login_with_facebook_valid(self):
+        """
+        Test the login with a valid Facebook account.
+        :return: None
+        """
+        self._type_facebook_email(os.getenv("FACEBOOK_EMAIL"))
+        self._type_facebook_password(os.getenv("FACEBOOK_PASSWORD"))
+        self._click_on_login_button()
+
+        # Click on the continue as button.
+        continue_as_button: WebElement = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//span[contains(normalize-space(.), 'Lanjutkan sebagai')]")
+            )
+        )
+        continue_as_button.click()
+
+        # Switch back to the Lazada window
+        self._switch_window(0)
+
+        # BUG: Kayaknya yang ini problematik, soalnya text Welcome back ga bakal muncul.
+        # Wait for the welcome back text to appear.
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[contains(text(), 'Welcome back')]")
+            )
+        )
+
+        assert (
+            "https://www.lazada.co.id/" in self.browser.current_url
+        ), "User should be redirected to the homepage"
