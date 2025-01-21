c9de1055c7ba256b10e289dae93bad6a96a5daed
RedFrosy <112476864+RedFrosy@users.noreply.github.com>
2025-01-21 21:16:13 +0700

    add checkout

--- src/test/kotlin/com/softwaretesting/lazadawebtesting/checkout/CheckoutPageTest.kt
+++ src/test/kotlin/com/softwaretesting/lazadawebtesting/checkout/CheckoutPageTest.kt
@@ -64,9 +64,24 @@ class CheckoutPageTest {
             )
         )
         cartButton.click()
-        wait.until(ExpectedConditions.urlContains("cart"))
 
-        cartPage.checkCheckbox(1)
+        val checkbox = wait.until(
+            ExpectedConditions.elementToBeClickable(
+                By.cssSelector(".next-checkbox.list-header-checkbox")
+            )
+        )
+
+        // Click the checkbox
+        if (!checkbox.isSelected) { // Check if the checkbox is not already selected
+            checkbox.click()
+            println("The checkbox was clicked successfully.")
+        } else {
+            println("The checkbox is already selected.")
+        }
+
+//        wait.until(ExpectedConditions.urlContains("cart"))
+//
+//        cartPage.checkCheckbox(1)
 
         val checkoutButton = driver.findElement(By.cssSelector(".checkout-order-total-button"))
         checkoutButton.click()
@@ -77,17 +92,55 @@ class CheckoutPageTest {
         paymentMethod.click()
 
         // The container
-        val paymentMethods = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".methods")))
-        paymentMethods.findElement(By.xpath("//*[text()='QRIS']"))
+//        val paymentMethods = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".methods")))
+//        paymentMethods.findElement(By.xpath("//*[text()='QRIS']"))
+//
+//        val payButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn")))
+//        payButton.click()
 
-        val payButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn")))
-        payButton.click()
+        // Locate the <div> containing "QRIS"
+        val qrisDiv = wait.until(
+            ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[contains(@class, 'method-item')]//div[contains(text(), 'QRIS')]")
+            )
+        )
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@text()='Buat pesanan']"))).click()
+        // Locate the parent <div> of the "QRIS" element
+        val parentDiv = qrisDiv.findElement(By.xpath("./ancestor::div[contains(@class, 'method-item')]"))
 
-        wait.until(ExpectedConditions.urlContains("paymen-cashier-result"))
+        // Click the parent <div>
+        parentDiv.click()
 
-        val qrCode = driver.findElement(By.cssSelector(".barcode"))
+        val confirmButton = wait.until(
+            ExpectedConditions.elementToBeClickable(
+                By.xpath("//div[@class='btn' and contains(text(), 'Konfirmasi pilihan')]")
+            )
+        )
+
+        // Click the "Konfirmasi pilihan" button
+        confirmButton.click()
+
+//        what is this for?
+//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@text()='Buat pesanan']"))).click()
+//
+//        wait.until(ExpectedConditions.urlContains("paymen-cashier-result"))
+
+        val orderButton = wait.until(
+            ExpectedConditions.elementToBeClickable(
+                By.xpath("//div[contains(text(), 'Buat pesanan')]")
+            )
+        )
+
+        // Click the "Buat pesanan" button
+        orderButton.click()
+
+        val qrCode = wait.until(
+            ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("img.automation-barcode-image")
+            )
+        )
+
+//        val qrCode = driver.findElement(By.cssSelector(".barcode"))
         Assert.assertTrue(qrCode.isDisplayed)
     }
 }
